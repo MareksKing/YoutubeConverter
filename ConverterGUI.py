579732430ec590b0b509880a06fbbb1659e7cba5
Mareks <robaldsmareks@gmail.com>
2023-10-06 22:55:58 +0300

    Added packs, started validation methods

--- ConverterGUI.py
+++ ConverterGUI.py
@@ -1,5 +1,16 @@
 import customtkinter
 
+
+def validate():
+    global download_type_label
+    value = option.get()
+    if value == "Audio":
+        download_type_label.configure(text ="Downloading audio only")
+    elif value == "Video":
+        download_type_label.configure(text ="Downloading video and audio.")
+    else:
+        download_type_label.configure(text ="A download type must be selected.")
+        
 customtkinter.set_appearance_mode("Dark")
 customtkinter.set_default_color_theme("dark-blue")
 
@@ -7,23 +18,25 @@ root =customtkinter.CTk()
 root.geometry("600x400")
 
 frame = customtkinter.CTkFrame(master=root)
-frame.place()
+frame.pack(pady=20, padx=40, fill="both", expand=True)
 
 link_label = customtkinter.CTkLabel(master=frame, text="Youtube Link")
-link_label.place()
+link_label.pack(pady=12, padx=10)
 
 link_input = customtkinter.CTkEntry(master=frame, placeholder_text="Link here", width=400)
-link_input.place()
+link_input.pack(pady=12, padx=10)
 
 download_type_label = customtkinter.CTkLabel(master=frame, text="Select download type")
-download_type_label.place()
+download_type_label.pack(pady=12, padx=10)
+option = customtkinter.StringVar()
 
-download_type_audio = customtkinter.CTkRadioButton(master=frame, text="Audio only", value=1)
-download_type_audio.place()
+download_type_audio = customtkinter.CTkRadioButton(master=frame, text="Audio only", value="Audio",variable=option)
+download_type_audio.pack(pady=12, padx=10)
 
-download_type_video = customtkinter.CTkRadioButton(master=frame, text="Video & Audio", value=2)
-download_type_video.place()
+download_type_video = customtkinter.CTkRadioButton(master=frame, text="Video & Audio", value="Video", variable=option)
+download_type_video.pack(pady=12, padx=10)
 
-download_button = customtkinter.CTkButton(master=frame, text="Download")
-download_button.place()
+download_button = customtkinter.CTkButton(master=frame, text="Download", command=validate)
+download_button.pack(pady=12, padx=10)
+
 root.mainloop()
