b4436826bbbc70b08d6a00b9fccb456dde9bcf86
Mareks <robaldsmareks@gmail.com>
2023-10-07 01:38:32 +0300

    validation, replaced radio buttons with segmented button

--- ConverterGUI.py
+++ ConverterGUI.py
@@ -1,16 +1,27 @@
 import customtkinter
 
 
-def validate():
-    global download_type_label
-    value = option.get()
-    if value == "Audio":
-        download_type_label.configure(text ="Downloading audio only")
-    elif value == "Video":
-        download_type_label.configure(text ="Downloading video and audio.")
+#Return true if link input is not empty
+def validate_link_not_empty():
+    youtube_link = link_input.get()
+    return bool(youtube_link)
+
+#Return true if download type is selected
+def validate_download_type():
+    print(download_type_segmented_button.get())
+
+def validate_and_run():
+    validate_download_type()
+    validate_link_not_empty()
+    
+
+def select_folder():
+    selected_folder = customtkinter.filedialog.askdirectory(title="Select download folder")
+    if selected_folder:
+        download_location_input.configure(placeholder_text=selected_folder)
     else:
-        download_type_label.configure(text ="A download type must be selected.")
-        
+        print("No folder selected")
+
 customtkinter.set_appearance_mode("Dark")
 customtkinter.set_default_color_theme("dark-blue")
 
@@ -26,17 +37,20 @@ link_label.pack(pady=12, padx=10)
 link_input = customtkinter.CTkEntry(master=frame, placeholder_text="Link here", width=400)
 link_input.pack(pady=12, padx=10)
 
-download_type_label = customtkinter.CTkLabel(master=frame, text="Select download type")
-download_type_label.pack(pady=12, padx=10)
-option = customtkinter.StringVar()
+download_location_input = customtkinter.CTkEntry(master=frame, placeholder_text="Select download location", width=200)
+download_location_input.pack(pady=12, padx=10)
 
-download_type_audio = customtkinter.CTkRadioButton(master=frame, text="Audio only", value="Audio",variable=option)
-download_type_audio.pack(pady=12, padx=10)
+download_location_button = customtkinter.CTkButton(master=frame, text="Download location", command=select_folder)
+download_location_button.pack(pady=12, padx=10)
 
-download_type_video = customtkinter.CTkRadioButton(master=frame, text="Video & Audio", value="Video", variable=option)
-download_type_video.pack(pady=12, padx=10)
+download_type_segmented_button = customtkinter.CTkSegmentedButton(master=frame, values=["Audio only","Video & Audio"], width=600)
+download_type_segmented_button.set("Audio only")
+download_type_segmented_button.pack(pady=12, padx=10)
 
-download_button = customtkinter.CTkButton(master=frame, text="Download", command=validate)
+
+download_button = customtkinter.CTkButton(master=frame, text="Download", command=validate_and_run)
 download_button.pack(pady=12, padx=10)
+
+
 
 root.mainloop()
