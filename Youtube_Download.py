7108e985d0846ccb23caf1fab81ac7595144a3c4
Mareks <robaldsmareks@gmail.com>
2023-10-10 01:42:45 +0300

    Video downloads work for 1080p 30fps, but not for 60fps

--- Youtube_Download.py
+++ Youtube_Download.py
@@ -1,5 +1,7 @@
 from pytube import YouTube
 import argparse
+import os
+import subprocess
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--link", help="Youtube link")
@@ -17,8 +19,15 @@ if download_type == "Audio only":
     audio.download(location, filename_prefix="Audio")
     print("Done!")
 else:
+    audio = yt.streams.get_audio_only()
+    audio.download(location, filename="audiotemp.mp3")
+    print("Audio done")
     video = yt.streams.get_by_itag(137)
-    # video = yt.streams.filter(progressive=True).las
-    video.download(location, filename_prefix="Video")
+    video.download(location, filename="videotemp.mp4")
+    print("Video done")
+    os.chdir(location)
+    subprocess.call(['ffmpeg', '-i', 'videotemp.mp4','-i','audiotemp.mp3', '-c:v', 'copy', '-c:a', 'aac', f'{video.title}.mp4'])
+    os.remove(location+"/videotemp.mp4")
+    os.remove(location+"/audiotemp.mp3")
     print("Download done!")
 
